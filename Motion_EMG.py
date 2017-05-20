d54dfcbc8041d770612a0eba4bd2203275efd004
HaoHuaqing <534390598@qq.com>
2017-05-20 23:31:41 +0800

    add resample funcation and time information

--- Motion_EMG.py
+++ Motion_EMG.py
@@ -4,7 +4,7 @@ import os.path
 import pandas as pd
 import csv
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy import interpolate
 EMG_folder = os.listdir('C:\\code\\EEG_Motion\\Subject_16_EXP1\\FES_Subject_16\\')
 Motion_folder = os.listdir('C:\\code\\EEG_Motion\\Subject_16_EXP1\\Subject16_e1\\')
 
@@ -37,7 +37,7 @@ def loadMotion(Motion_file):
 # EMG_title = EMG_title[0]
 def title():
     EMG_title = np.array(
-        ['PECTORALIS MAJOR: EMG 1', 'POSTERIOR DELTOID: EMG 2', 'ANTERIOR BRACHII: EMG 3', 'BICEPS BRACHII: EMG 4',
+        ['Time', 'PECTORALIS MAJOR: EMG 1', 'POSTERIOR DELTOID: EMG 2', 'ANTERIOR BRACHII: EMG 3', 'BICEPS BRACHII: EMG 4',
          'TRICEPS BRACHII LATERAL: EMG 5', 'TRICEPS BRACHII LONG: EMG 6', 'BRACHIORADIALIS: EMG 7',
          'EVENT MARKER: EMG 16'])
     Motion_title = np.array(
@@ -66,26 +66,55 @@ def process(EMG_data, Motion_data):
     # print(EMG_trigger_list[-1])
 
     # EMG:1927HZ  Motion:120HZ  1927/120 = 16
-    # Sample EMG every 16 points
-    EMG_processed_data = np.array([[v for i, v in enumerate(EMG_data[:, 7]) if i % 16 == 0]])
-    for count in range(1, 8):
-        x = np.array([[v for i, v in enumerate(EMG_data[:, 7 - count]) if i % 16 == 0]])
+    # resample Motion to 1000hz
+    EMG_processed_data = np.array([[v for i, v in enumerate(EMG_data[:, 7]) if i % 2 == 0]])
+    for count in range(1,8):
+        x = np.array([[v for i, v in enumerate(EMG_data[:, 7 - count]) if i % 2 == 0]])
         EMG_processed_data = np.concatenate((EMG_processed_data, x), axis=0)
+    Time_data = np.array([[i / 1000.0 for i in range(EMG_processed_data.shape[1])]])
+    EMG_processed_data = np.concatenate((EMG_processed_data, Time_data), axis=0)
     EMG_processed_data = np.rot90(EMG_processed_data, 3)
-    EMG_processed_data = EMG_processed_data[:Motion_data.shape[0]]
 
-    Merged_data = np.concatenate((EMG_processed_data, Motion_data), axis=1)
+    # Motion data interpolation
+    x = np.linspace(0, len(Motion_data[:, 14]), len(Motion_data[:, 14]))
+    y = Motion_data[:, 14]
+    f = interpolate.interp1d(x, y, kind='linear')
+    xnew = np.linspace(0, len(Motion_data[:, 14]), int(len(Motion_data[:, 14]) / 120 * 1000))
+    Motion_processed_data = np.array([f(xnew)])
+    for count in range(1,15):
+        x = np.linspace(0, len(Motion_data[:, 14 - count]), len(Motion_data[:, 14 - count]))
+        y = Motion_data[:, 14 - count]
+        f = interpolate.interp1d(x, y, kind='linear')
+        xnew = np.linspace(0, len(Motion_data[:, 14 - count]), int(len(Motion_data[:, 14 - count]) / 120 * 1000))
+        ynew = np.array([f(xnew)])
+        Motion_processed_data = np.concatenate((Motion_processed_data, ynew), axis=0)
+    Motion_processed_data = np.rot90(Motion_processed_data, 3)
+    Motion_processed_data = Motion_processed_data[:EMG_processed_data.shape[0]]
+    Merged_data = np.concatenate((EMG_processed_data, Motion_processed_data), axis=1)
     return Merged_data
 
 
 if __name__ == "__main__":
+    FR_num = 10
     if len(EMG_folder) == len(Motion_folder):
-        for count in range(len(EMG_folder)):
+        # data belong to FR and LR
+        for count in range(FR_num):
             EMG_data = loadEMG(EMG_folder[count])
             Motion_data = loadMotion((Motion_folder[count]))
             Merged_title = title()
             Merged_data = process(EMG_data, Motion_data)
-            with open('EMG_Motion' + str(count) + '.csv', 'w', newline='') as f:
+            with open('EMG_Motion_FR' + str(count) + '.csv', 'w', newline='') as f:
+                f_csv = csv.writer(f)
+                f_csv.writerow(Merged_title)
+                f_csv.writerows(Merged_data)
+            print(str(count + 1) + ' of ' + str(len(EMG_folder)) + ' completed')
+
+        for count in range(FR_num, len(EMG_folder)):
+            EMG_data = loadEMG(EMG_folder[count])
+            Motion_data = loadMotion((Motion_folder[count]))
+            Merged_title = title()
+            Merged_data = process(EMG_data, Motion_data)
+            with open('EMG_Motion_LR' + str(count - FR_num) + '.csv', 'w', newline='') as f:
                 f_csv = csv.writer(f)
                 f_csv.writerow(Merged_title)
                 f_csv.writerows(Merged_data)
