c1df757f5c6784621e93ae15603d77ce64777069
HaoHuaqing <534390598@qq.com>
2017-07-27 22:31:57 +0800

    bug fix

--- Synergy_plot.m
+++ Synergy_plot.m
@@ -1,6 +1,6 @@
 average_2 = 0;
 average_4 = 0;
-for i = 1:10;
+for i = 1:LengthFiles-2;
     subplot(2,2,2)
     plot(Synergy_S(2).C(1600*(i-1)+1:1600*i,1),'Color',[96 96 96]/255);
     hold on
@@ -15,8 +15,8 @@ bar(Synergy_S(2).U(1,:),'y','Horizontal','on')
 subplot(2,2,3)
 bar(Synergy_S(2).U(end,:),'y','Horizontal','on')
 subplot(2,2,2)
-plot(average_2/10,'Color',[1 0 0], 'LineWidth',4)
+plot(average_2/(LengthFiles-2),'Color',[1 0 0], 'LineWidth',4)
 subplot(2,2,4)
-plot(average_4/10,'Color',[1 0 0], 'LineWidth',4)
+plot(average_4/(LengthFiles-2),'Color',[1 0 0], 'LineWidth',4)
 h = suptitle('S11 LR PRE');
 set(h,'Fontsize',30);
